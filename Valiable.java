c36edf450ac62a74eba69d3eda31a88a0ef27493
AoiKubota <jaxaesagoo@gmail.com>
2019-06-18 15:34:33 +0900

    added Valiable.java
    This code written how to use valiable.

---
+++ Valiable.java
@@ -0,0 +1,12 @@
+public class Valiable {
+	public static void main(String[] args) {
+		int a = 1;
+		String b = "hey";
+		float c = 3.14F;
+		boolean d = false;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
