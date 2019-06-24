2281d1494a15b27ee4aefd82087061df89dbe3fa
AoiKubota <jaxaesagoo@gmail.com>
2019-06-24 22:47:31 +0900

    added Method.java
    This code written how  to use method.

---
+++ Source codes/Method.java
@@ -0,0 +1,10 @@
+public class Method { 
+	public static void main(String[] args) {
+		System.out.println("I started method.");
+		abcde("Aoi");
+		abcde("Kubota");
+	}
+	public static void abcde(String name) {
+		System.out.println("You are " + name);
+	}
+}
